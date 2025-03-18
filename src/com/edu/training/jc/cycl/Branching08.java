15b5b6d04559fd00a35cdfda6afe39de22c3775e
Aleksandr Klimov <70091133+blik1984@users.noreply.github.com>
2025-03-18 10:56:23 +0300

    Update Branching08.java

--- src/com/edu/training/jc/cycl/Branching08.java
+++ src/com/edu/training/jc/cycl/Branching08.java
@@ -11,7 +11,11 @@ public class Branching08 {
 		int num1 = -455;
 		int num2 = 8741618;
 
-		if (num1 * num1 < num2 * num2) {
+		if (Math.abs(num1) > (Integer.MAX_VALUE / Math.abs(num1)) || Math.abs(num2) > (Integer.MAX_VALUE / Math.abs(num2))) {
+			System.out.println("Посчитать невозможно");
+		} else if (num1 == num2) {
+			System.out.println("Числа равны");
+		} else if (num1 * num1 < num2 * num2) {
 			System.out.println("Квадрат числа  " + num1 + " наименьшйи"); // другой вариант это взять модули чисел и сравнить их
 		} else {
 			System.out.println("Квадрат числа  " + num2 + " наименьшйи");
